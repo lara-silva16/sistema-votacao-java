31671a9813964dff6cb0afdde4d7b617046dd0f1
Lara Pereira Martins da Silva <lara.11881479@aluno.mg.gov.br>
2026-09-02 11:09:58 -0300

    feat: listar candidatos disponíveis para votação

--- src/SistemaVotacao.java
+++ src/SistemaVotacao.java
@@ -30,6 +30,15 @@ public class SistemaVotacao{
 
         return indiceEncontrado;
     }
+    static void mostrarCandidatos() {
+        System.out.println("\nCandidatos disponíveis:");
+
+        for (int i = 0; i < quantidadeCandidatos; i++) {
+            System.out.println(
+                    numerosCandidatos[i] + " - " + nomesCandidatos[i]
+            );
+        }
+    }
 
 
     public static void main(String[] args){
